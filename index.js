8546b023c66ca3387eef16c36c96b5444e189935
henryijs <henrycheng@instajs.com>
2022-05-23 19:30:48 +0800

    Update hash with directory

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
 const ipfsCidImporter = require('ipfs-unixfs-importer')
+const MemoryBlockstore = require('blockstore-core/memory')
 const fs = require('fs')
 const path = require('path')
 
-async function getCid(path) {
-  const content = fs.readFileSync(path)
-  const block = {
-    get: async cid => { throw new Error(`unexpected block API get for ${cid}`) },
-    put: async () => { throw new Error('unexpected block API put') }
-  }
-  if (typeof content === 'string') {
-    content = new TextEncoder().encode(content)
-  }
+async function getCid(filepath) {
+  const blockstore = new MemoryBlockstore.MemoryBlockstore()
+  const source = [{
+    path: filepath,
+    //content: fs.createReadStream(path)
+  }]
+  //const content = fs.readFileSync(path)
+  //const block = {
+  //  get: async cid => { throw new Error(`unexpected block API get for ${cid}`) },
+  //  put: async () => { throw new Error('unexpected block API put') }
+  // }
+  // if (typeof content === 'string') {
+  //   content = new TextEncoder().encode(content)
+  // }
 
-  for await (const cid of ipfsCidImporter.importer([{ content }], block, { onlyHash: true })) {
+  //for await (const cid of ipfsCidImporter.importer([{ content }], block, { onlyHash: true })) {
+    for await (const cid of ipfsCidImporter.importer(source, blockstore, { onlyHash: true, wrapWithDirectory: true })) {
     console.log(cid.cid)
   }
 }
 
 // Get content's cid
-getCid(path.resolve('./test.txt'))
+getCid(path.resolve('./test'))
 
