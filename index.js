548f48fd1cf6c3596ef5ee0b83df2e26b728f03b
henryijs <henrycheng@instajs.com>
2022-06-01 12:41:39 +0800

    Update use dagPB to hash

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const ipfsCidImporter = require('ipfs-unixfs-importer')
 const MemoryBlockstore = require('blockstore-core/memory')
 const Unixfs = require('ipfs-unixfs')
+const CID = require('multiformats/cid')
+const hasher = require('multiformats/hashes/sha2')
+const dagPB = require('@ipld/dag-pb')
 const fs = require('fs')
 const path = require('path')
+//const { Hash } = require('crypto')
 
-const testDirectoryCid = 'QmXwnkFTQ2RZWKygXqVemyZsbB2eNwGDknpP7Zw8gYfnf3'
+/* const testDirectoryCid = 'QmXwnkFTQ2RZWKygXqVemyZsbB2eNwGDknpP7Zw8gYfnf3'
 const testTxtCid = 'QmVj6Nc12T2DppxorEzFX5U9GsyWTEqBDxt7d4AWSotxv7'
 
 
@@ -31,22 +35,22 @@ async function getDirectoryCid(dirPath, fileName) {
   const blockstore = new MemoryBlockstore.MemoryBlockstore()
   const unixfs = new Unixfs.UnixFS({ type: 'file'})
   const source = [{
-    cid: 'QmZXiu2EJ952cR4MgPi8o9NJbbhk88zxD4TtGHHjjyFiWB',
+    cid: CID.CID.parse('QmZXiu2EJ952cR4MgPi8o9NJbbhk88zxD4TtGHHjjyFiWB'),
     path: 'IJS_Tech.svg',
-    unixfs,
-    size: 9882
+    unixfs: unixfs.marshal(),
+    //node: 'DAGNode'
   },
   {
-    cid: 'Qmc1HH1EhtHYScvocjPTp6DXz8rcEBJRKBzi4jtEHrWXUE',
+    cid: CID.CID.parse('Qmc1HH1EhtHYScvocjPTp6DXz8rcEBJRKBzi4jtEHrWXUE'),
     path: 'icon.svg',
-    unixfs,
-    size: 3245
+    unixfs: unixfs.marshal(),
+    //node: 'DAGNode'
   },
   {
-    cid: 'QmSq2AyU9jPfUvTdCRsd6CL3RBqVUZbUYdvKHN9kfTVkRY',
+    cid: CID.CID.parse('QmSq2AyU9jPfUvTdCRsd6CL3RBqVUZbUYdvKHN9kfTVkRY'),
     path: 'tutor_Logo2.PNG',
-    unixfs,
-    size: 33040
+    unixfs: unixfs.marshal(),
+    //node: 'DAGNode'
   }
     //path: dirPath,
     //content: fs.createReadStream(fileName)
@@ -60,13 +64,54 @@ async function getDirectoryCid(dirPath, fileName) {
   //   content = new TextEncoder().encode(content)
   // }
 
+  console.log(source)
   //for await (const cid of ipfsCidImporter.importer([{ content }], block, { onlyHash: true })) {
-  for await (const cid of ipfsCidImporter.importer(source, blockstore, { onlyHash: true, wrapWithDirectory: true })) {
+  for await (const cid of ipfsCidImporter.importer(source, blockstore, { onlyHash: true, wrapWithDirectory: true,  })) {
     console.log(cid)
   }
+} */
+
+async function getDirectoryCid2() {
+  const unixfs = new Unixfs.UnixFS({ type: 'directory'})
+  const bytes = dagPB.encode(dagPB.prepare({
+    Data: unixfs.marshal(),
+    Links: [
+      {
+        Hash: CID.CID.parse('QmZXiu2EJ952cR4MgPi8o9NJbbhk88zxD4TtGHHjjyFiWB'),
+        Name: 'IJS_Tech.svg',
+        Tsize: 9882 + 8
+      },
+      {
+        Hash: CID.CID.parse('Qmc1HH1EhtHYScvocjPTp6DXz8rcEBJRKBzi4jtEHrWXUE'),
+        Name: 'icon.svg',
+        Tsize: 3245 + 8
+      },
+      {
+        Hash: CID.CID.parse('QmSq2AyU9jPfUvTdCRsd6CL3RBqVUZbUYdvKHN9kfTVkRY'),
+        Name: 'tutor_Logo2.PNG',
+        Tsize: 33040 + 8
+      }
+    ]
+  }))
+
+  //console.log(bytes)
+  const hash = await hasher.sha256.digest(bytes)
+  const cid = CID.CID.create(0, dagPB.code, hash)
+
+  console.log(cid, '=>', Buffer.from(bytes).toString('hex'))
+
+  const decoded = dagPB.decode(bytes)
+
+  console.log(decoded)
+  //console.log(`decoded "Data": ${new TextDecoder().decode(decoded.Data)}`)
+  console.log(cid);
+  const file1Length = fs.readFileSync(path.join(process.cwd(), 'test', '_media', 'icon.svg')).length
+  const file2Length = fs.readFileSync(path.join(process.cwd(), 'test', '_media', 'tutor_Logo2.PNG')).length
+  const file3Length = fs.readFileSync(path.join(process.cwd(), 'test', '_media', 'IJS_Tech.svg')).length
+  console.log(file1Length, file2Length, file3Length)
 }
 
-async function getCid(filepath) {
+/* async function getCid(filepath) {
   const blockstore = new MemoryBlockstore.MemoryBlockstore()
   const source = [{
     path: filepath,
@@ -85,9 +130,9 @@ async function getCid(filepath) {
   for await (const cid of ipfsCidImporter.importer(source, blockstore, { onlyHash: true, wrapWithDirectory: true })) {
     console.log(cid.cid)
   }
-}
+} */
 
 // Get content's cid
 //createFileSourceArray('./test')
-getDirectoryCid()
+getDirectoryCid2()
 
