85dd7ae4c32bf2c0c6a366d1de4de33d54cd0397
henryijs <henrycheng@instajs.com>
2022-06-06 17:21:43 +0800

    Update array TBD

--- index.js
+++ index.js
@@ -2,13 +2,55 @@ const CID = require('multiformats/cid')
 const Unixfs = require('ipfs-unixfs')
 const hasher = require('multiformats/hashes/sha2')
 const dagPB = require('@ipld/dag-pb')
+const ipfsClient = require('ipfs-http-client')
 
+function createIpfsFileSourceArray(sourceCid) {
+  const sourceArray = new Array()
+  sourceArray.push(await getDirectoryCid(sourceCid))
+  console.log(sourceArray)
+  return sourceArray
+}
 
-async function getDirectoryCid() {
+function listSubDirectoryFile(sourceCid) {
+  const dirList = fs.readdirSync(dirPath).forEach(file => {
+    const absolute = path.join(dirPath, file)
+    if (fs.statSync(absolute).isDirectory()) {
+      return listSubDirectoryFile(absolute)
+    } else {
+      console.log(dirPath + ' ' + file)
+      return { dirPath, file }
+    }
+  })
+}
+
+async function getDirectoryCid(sourceCid) {
+  const ipfs = await ipfsClient.create('http://localhost:5001/api/v0')
+  //const ipfsFileArray = new Array()
+  for await (const file of ipfs.ls(sourceCid)) {
+    console.log(file)
+    if (file.type === 'dir') {
+      return getDirectoryCid(file.cid)
+    } else {
+      const stat = await ipfs.files.stat('/ipfs/' + file.cid)
+      console.log('cumulativeSize: ' + stat.cumulativeSize)
+      return {
+        Hash: file.cid.toString(),
+        Name: file.name,
+        Tsize: stat.cumulativeSize
+      }
+    }
+  }
+  //console.log(ipfsFileArray)
+}
+
+async function hashCid(sourceCid) {
+  const fileArray = new Array()
+  fileArray.push(await createIpfsFileSourceArray(sourceCid))
+  console.log(fileArray)
   const unixfs = new Unixfs.UnixFS({ type: 'directory' })
   const bytes = dagPB.encode(dagPB.prepare({
     Data: unixfs.marshal(),
-    Links: [
+    Links: fileArray/* [
       // Hash = CID, Name = Filename with sub-dir, Tsize = File size in DAG-PB IPLD - cumulativeSize (uint64)
       {
         Hash: CID.CID.parse('QmZXiu2EJ952cR4MgPi8o9NJbbhk88zxD4TtGHHjjyFiWB'),
@@ -25,18 +67,23 @@ async function getDirectoryCid() {
         Name: 'tutor_Logo2.PNG',
         Tsize: 33054
       }
-    ]})
+    ] */
+    })
   )
   //console.log(bytes)
   const hash = await hasher.sha256.digest(bytes)
   const cid = CID.CID.create(0, dagPB.code, hash)
-  console.log(cid, '=>', Buffer.from(bytes).toString('hex'))
+  //console.log(cid, '=>', Buffer.from(bytes).toString('hex'))
   const decoded = dagPB.decode(bytes)
-  console.log(decoded)
-  console.log(cid)
+  //console.log(decoded)
+  //console.log(cid)
   return cid.toString()
 }
 
 // test/_media
-// const testDirectoryCid = 'QmQbwtYnN12SQEJHpRm8UztMD8LbqAssxt2jMeDFceiJVz'
-const hashedCid = getDirectoryCid()
+const testDirectoryCid = 'QmQbwtYnN12SQEJHpRm8UztMD8LbqAssxt2jMeDFceiJVz'
+const newCid = hashCid('QmVW7mXm2KGipfEC9w3k3h56bxedmxbmdiY3hxdyQFLeMF').toString()
+console.log(newCid)
+if (newCid === testDirectoryCid) {
+  console.log(true)
+}
